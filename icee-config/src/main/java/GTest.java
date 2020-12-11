6a4fb80612a80369730a7f456e2483050cd5e69e
precorrect <L_hire@163.com>
2020-12-11 22:25:17 +0800

    Update GTest.java

--- icee-config/src/main/java/GTest.java
+++ icee-config/src/main/java/GTest.java
@@ -1,16 +1,17 @@
 public class GTest {
     public static void main(String[] args) {
         GTest gTest = new GTest();
-        System.out.println("测试结构: " + gTest.add(9, 34));
+        System.out.println("test git checkout -- <file>")
+        System.out.println("test result: " + gTest.add(9, 34));
     }
 
     public int add(int a, int b) {
         try {
             return a + b;
         } catch (Exception e) {
-            System.out.println("catch 测试");
+            System.out.println("catch test");
         } finally {
-            System.out.println("finally 测试");
+            System.out.println("finally test");
         }
         return 0;
     }
